109e2283ae88915d32a86447158a97afa64aceb8
NephygTdkingz <tdkingzict@gmail.com>
2025-08-14 16:39:28 +0100

    made first preparation for vercel

--- stanbex_global/urls.py
+++ stanbex_global/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,3 +10,4 @@ urlpatterns = [
     path('account/customer/', include('customer.urls', namespace="customer")),
     path('account/transaction/', include('transaction.urls', namespace="transaction")),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
